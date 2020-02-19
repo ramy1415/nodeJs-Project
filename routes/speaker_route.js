b7f22253336a7a0dd559f4e0fe40f5e8869d84f2
ramy1415 <59793221+ramy1415@users.noreply.github.com>
2020-02-19 07:14:52 +0200

    adding session control and adding liks to front end to add/delete/edit/list for both events and speakers , also adding logout button

--- routes/speaker_route.js
+++ routes/speaker_route.js
@@ -7,49 +7,103 @@ const path=require('path')
 speaker.use(express.urlencoded({extended:true}));
 
 
-speaker.get('/profile',(request,response)=>{
-    response.send("profile")
+speaker.use((request,response,next)=>{
+    response.locals.UserName=request.session.UserName
+    next()
 })
-speaker.get('/add',(request,response)=>{
-    // response.sendFile(path.join(__dirname, '../', 'views/add.html'));
-    response.render('speakers/register.ejs')
+
+speaker.get('/profile',(request,response)=>{
+    response.render('speakers/profile.ejs')
+})
+
+
+speaker.use((request,response,next)=>{
+    if(request.session.role=="admin")
+        next()
+    else
+        response.redirect('profile')
+})
+speaker.get('/add',(request,response,next)=>{
+    response.render('speakers/add.ejs')
 })
 speaker.post('/add',(request,response)=>{
     let newSpeaker=new mongoose.model('speaker')(
         request.body
     )
     newSpeaker.save().then((data)=>{
-        response.send("saved")
+        response.redirect('/speaker/list')
     }).catch((error)=>{
         response.send(error+"")
     })
 })
 speaker.get('/list',(request,response)=>{
-    mongoose.model('speaker').find({}).then((data)=>{
-        response.send(data)
+    mongoose.model('speaker').find({}).then((speakers_details)=>{
+        response.render('speakers/list.ejs',{speakers_details})
     }).catch((error)=>{
         response.send(""+error)
     })
 })
-speaker.post('/edit',(request,response)=>{
-    mongoose.model('speaker').updateOne({_id:request.body._id},{ $set: {name:request.body.name} },function(err, res) {
+
+speaker.get('/editthis/:_id',(request,response)=>{
+    let id=request.params._id
+    mongoose.model('speaker').findOne({ _id: id }).then((data)=>{
+        response.render('speakers/editthis.ejs',{data})
+    })
+})
+
+speaker.post('/editThis',(request,response)=>{
+    mongoose.model('speaker').updateOne({_id:request.body._id},{ $set: {
+        FullName:request.body.FullName,
+        UserName:request.body.UserName,
+        Password:request.body.Password,
+        Address:{
+            city:request.body.city,
+            street:request.body.street,
+            building:request.body.building
+        }
+    } },function(err, res) {
         if (err) throw err;
-        console.log("1 document updated");
-        // db.close();
+        response.redirect('/speaker/list')
       })
 })
-speaker.get('/delete',(request,response)=>{
+speaker.get('/delete/',(request,response)=>{
     mongoose.model('speaker').find({},{_id:1,UserName:1}).then((speakers)=>{
         response.render('speakers/delete.ejs',{speakers})
     }).catch((error)=>{
         response.send(""+error)
     })
 })
+speaker.get('/remove/:_id',(request,response)=>{
+    let id=request.params._id
+    mongoose.model('speaker').deleteOne({ _id: id }, function(err, obj) {
+        if (err) throw err;
+        response.redirect('/speaker/list')
+      })
+})
 
 speaker.post('/delete',(request,response)=>{
     mongoose.model('speaker').deleteOne({ _id: request.body._id }, function(err, obj) {
         if (err) throw err;
-        response.send('deleted')
+        response.redirect('/speaker/list')
       })
+})
+
+
+speaker.get('/edit', (request, response) => {
+    let id=request.params._id
+    mongoose.model('speaker').find({}).then((speakers_data)=>{
+        response.render('speakers/edit.ejs',{speakers_data})
+    }).then((error)=>{
+        console.log(""+error)
+    })
+})
+
+
+speaker.post('/edit', (request, response) => {
+    let id=request.body._id
+    console.log(id)
+    mongoose.model('speaker').findOne({ _id: id }).then((data)=>{
+        response.render('speakers/editthis.ejs',{data})
+    })
 })
 module.exports=speaker
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const server=express();
 const authenticator=require('./routes/authenticator_route');
 const speaker_route=require('./routes/speaker_route');
 const events_route=require('./routes/events_route');
+const session=require('express-session')
 server.set('view-engine','ejs')
+server.use(session({
+    secret:"ramy"
+}))
 let path = require('path');
 let mongoose=require('mongoose');
-
 server.use(express.static(path.join(__dirname,'/public')))
-
+server.use(express.static(path.join(__dirname,'node_modules/jquery/dist')))
+server.use(express.static(path.join(__dirname,'node_modules/bootstrap/dist')))
 mongoose.connect("mongodb://localhost:27017/eventsdb",{useNewUrlParser: true,useUnifiedTopology: true}).then((data)=>{
     console.log("connected")
     console.log(path.join(__dirname,'/public'))
@@ -17,12 +21,17 @@ mongoose.connect("mongodb://localhost:27017/eventsdb",{useNewUrlParser: true,use
 })
 server.listen(8082);
 
-server.get('',(request,response)=>{
-    // response.sendFile(path.join(__dirname, '../', 'views/addevent.html'))
-    response.render('login.ejs')
+server.get('',(request,response,next)=>{
+    response.render('speakers/login.ejs')
 })
 
 server.use(authenticator)
+server.use((request,response,next)=>{
+    if(request.session.role)
+        next()
+    else
+        response.redirect('/login')
+})
 server.use('/speaker',speaker_route)
 server.use('/event',events_route)
 
